0992cc78715aa4f90f055cac5116037e82f7ef6b
Xavier Montillet <xavierm02.net@gmail.com>
2018-03-25 22:25:56 +0200

    Used sudo for linking init.d files.

--- pi_setup.py
+++ pi_setup.py
@@ -10,14 +10,14 @@ def ln_s(source, target):
 		
 
 def sudo_ln_s(source, target):
-	cmd = "ln -s " + source + " " + target
-	cmd_str = "ln -s " + py_run.uri(source) + " " + py_run.uri(target)
+	cmd = "sudo ln -s " + source + " " + target
+	cmd_str = "sudo ln -s " + py_run.uri(source) + " " + py_run.uri(target)
 	py_run.run(cmd, cmd_str=cmd_str, error_fatal=True)
 
 def setup_power_button():
 	# requires python3-rpi.gpio
 	py_run.run("sudo apt install python3-rpi.gpio")
-	ln_s(os.path.realpath("./power_button/power_button.rc"), "/etc/init.d/power_button.rc")
+	sudo_ln_s(os.path.realpath("./power_button/power_button.rc"), "/etc/init.d/power_button.rc")
 	py_run.run("sudo update-rc.d power_button defaults")
 	py_run.run("sudo python /home/pi/scripts/shutdown.py &> /dev/null &")
 	
