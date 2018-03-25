60768e517be52b8d8b3ad9a9dc22cf8761eaf19d
Xavier Montillet <xavierm02.net@gmail.com>
2018-03-26 00:24:04 +0200

    Changed ln -sf to cp -f.

--- pi_setup.py
+++ pi_setup.py
@@ -3,20 +3,14 @@
 import lib.py_run as py_run
 import os
 
-def ln_s(source, target):
-	cmd = "ln -s " + source + " " + target
-	cmd_str = "ln -s " + py_run.uri(source) + " " + py_run.uri(target)
-	py_run.run(cmd, cmd_str=cmd_str, error_fatal=True)
-		
-
-def sudo_ln_s(source, target):
-	cmd = "sudo ln -s " + source + " " + target
-	cmd_str = "sudo ln -s " + py_run.uri(source) + " " + py_run.uri(target)
+def sudo_cp_f(source, target):
+	cmd = "sudo cp -f " + source + " " + target
+	cmd_str = "sudo cp -f " + py_run.uri(source) + " " + py_run.uri(target)
 	py_run.run(cmd, cmd_str=cmd_str, error_fatal=False)
 
 def setup_power_button():
 	# requires python3-rpi.gpio
-	sudo_ln_s(os.path.realpath("./power_button/power_button"), "/etc/init.d/power_button")
+	sudo_cp_f(os.path.realpath("./power_button/power_button"), "/etc/init.d/power_button")
 	py_run.run("sudo update-rc.d power_button defaults")
 	py_run.run("sudo python /home/pi/pi_setup/power_button/power_button.py &> /dev/null &")
 	
