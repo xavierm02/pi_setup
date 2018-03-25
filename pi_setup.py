cb243ec2528d0261b37299568b71bf652a23f1ee
Xavier Montillet <xavierm02.net@gmail.com>
2018-03-25 22:18:46 +0200

    Modified setup_power_button function.

--- pi_setup.py
+++ pi_setup.py
@@ -15,6 +15,10 @@ def sudo_ln_s(source, target):
 	py_run.run(cmd, cmd_str=cmd_str, error_fatal=True)
 
 def setup_power_button():
+	# requires python3-rpi.gpio
+	py_run.run("sudo apt install python3-rpi.gpio")
 	ln_s(os.path.realpath("./power_button/power_button.rc"), "/etc/init.d/power_button.rc")
+	py_run.run("sudo update-rc.d power_button defaults")
+	py_run.run("sudo python /home/pi/scripts/shutdown.py &> /dev/null &")
 	
 setup_power_button()
